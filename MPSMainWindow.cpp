afff98afe11dfc5c5a339f797db463f71f89ec13
Dan Munteanu <epsdel@gmail.com>
2025-06-29 15:27:02 +0300

    Adding copyright notices to each source file

--- MPSMainWindow.cpp
+++ MPSMainWindow.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of MemphisNow.
+ *
+ * MemphisNow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MemphisNow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MemphisNow.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "MPSMainWindow.hpp"
 #include "MPSCommon.hpp"
 #include "MPSTokenWidget.hpp"
